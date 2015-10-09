bf6a567a5d2cb00f485462ee946882ba9710acc2
jeffslutzky <jeffslutzky@gmail.com>
2015-10-09 15:41:36 -0400

    fixed deck

--- models/deck.rb
+++ models/deck.rb
@@ -1,15 +1,14 @@
 class Deck
 
-color_array = ["red", "blue", "green", "yellow"]
 
-(1..9).each do |number|
-  color_array.each do |color|
-    Card.new(number, color)
-  end
+	color_array = ["red", "blue", "green", "yellow"]
+
+	(1..9).each do |number|
+	  color_array.each do |color|
+	    Card.new(number, color)
+	  end
+	end
+
+
+
 end
-
-end
-
-deck = Deck.new
-
-puts Card.deck
