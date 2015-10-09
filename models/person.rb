7d5fe8259f64a6ce7c78e5ecf6e6890398cb7c3b
jeffslutzky <jeffslutzky@gmail.com>
2015-10-09 10:12:14 -0400

    create is_valid, begin to do the play method

--- models/person.rb
+++ models/person.rb
@@ -30,21 +30,21 @@ class Person
 
   def display_hand
     @hand.each_with_index do |card, index|
-      puts "#{index+1}. #{card.to_s}"
+      puts "(#{index+1}) --> #{card.to_s}"
     end
       puts "Type the number of the card you want to play, or 'skip' to draw a card and skip your turn."  
   end
 
   def is_valid?(card_played)
-    # check whether the play is_valid
+    @@last_card_played.color == card_played.color || @@last_card_played.number == card_played.number
   end
 
   def play
-    # display_hand
-    # puts last_card_played
+    puts "Last card played = #{@@last_card_played}"
+    display_hand
     # choice = gets.chomp
     # if choice == skip
-      # draw
+      # draw_a_card
     # elsif choice_is_valid(choice)
       # change last_card_played to choice
       # pop card from @hand
